dc6170e68def2945aa7c8e251e330f4b295a36d9
Jowam <69232719+dioame@users.noreply.github.com>
2021-08-17 13:42:24 +0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -36,7 +36,7 @@ $app->get('/version', function() use($app) {
       while (false !== ($entry = readdir($handle))) { $version = $entry; }
       closedir($handle);
 
-      return "<pre>".\Cowsayphp\Cow::say("Cool beans")."</pre>";
+      return 'app';
   }
 });
 $app->run();
